cbf0ff60bc11bc2db1d05ea0d22c8f48b643fd94
LuukvDamme <LuukvDamme@gmail.com>
2018-02-27 12:58:10 +0100

    Eindopdracht blok6 Luuk van Damme Exceptions toegevoegd

--- src/eindopdrachtblok6/Logic.java
+++ src/eindopdrachtblok6/Logic.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileSystemView;
 
 /**
@@ -68,10 +69,8 @@ public class Logic {
     public static ArrayList<String> Host() {
         for (int i = 1; i < Lijst.size(); i++) {
             Virus a = Lijst.get(i);
-            //if (HostLijst.contains(a.getHostTaxID()+" ("+a.getHostName()+")")){
             if (HostLijst.contains(a.getHostName())) {
             } else {
-                //Host=(a.getHostTaxID()+" ("+a.getHostName()+")");
                 Host = (a.getHostName());
                 HostLijst.add(Host);
                 Host = "";
@@ -80,307 +79,9 @@ public class Logic {
         return HostLijst;
     }
 
-    //////////////////////////////////
-    /*
-    //VirusLijst
-    public static ArrayList<String> Virus1(String HostName) {
-        //System.out.println(HostName);
-        VirusLijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus a = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(a.getHostName())) {
-                VirusLijst.add(a.getID());
-            }
-
-        }
-
-        //System.out.println(VirusLijst);
-        return VirusLijst;
-    }
-
-    public static ArrayList<String> Virus1SSDNA(String HostName) {
-        //System.out.println(HostName);
-        VirusLijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus a = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(a.getHostName())) {
-                if (a.getLineage().contains("ssDNA")) {
-                    VirusLijst.add(a.getID());
-                }
-
-            }
-        }
-        //System.out.println(VirusLijst);
-        return VirusLijst;
-    }
-
-    public static ArrayList<String> Virus1DSDNA(String HostName) {
-        //System.out.println(HostName);
-        VirusLijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus a = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(a.getHostName())) {
-                if (a.getLineage().contains("dsDNA")) {
-                    VirusLijst.add(a.getID());
-                }
-
-            }
-        }
-        //System.out.println(VirusLijst);
-        return VirusLijst;
-    }
-
-    public static ArrayList<String> Virus1SSRNA(String HostName) {
-        //System.out.println(HostName);
-        VirusLijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus a = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(a.getHostName())) {
-                if (a.getLineage().contains("ssRNA")) {
-                    VirusLijst.add(a.getID());
-                }
-
-            }
-        }
-        //System.out.println(VirusLijst);
-        return VirusLijst;
-    }
-
-    public static ArrayList<String> Virus1DSRNA(String HostName) {
-        //System.out.println(HostName);
-        VirusLijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus a = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(a.getHostName())) {
-                if (a.getLineage().contains("dsRNA")) {
-                    VirusLijst.add(a.getID());
-                }
-
-            }
-        }
-        //System.out.println(VirusLijst);
-        return VirusLijst;
-    }
-
-    public static ArrayList<String> Virus1Retro(String HostName) {
-        //System.out.println(HostName);
-        VirusLijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus a = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(a.getHostName())) {
-                if (a.getLineage().contains("Retro-transcribing viruses")) {
-                    VirusLijst.add(a.getID());
-                }
-
-            }
-        }
-        //System.out.println(VirusLijst);
-        return VirusLijst;
-    }
-
-    public static ArrayList<String> Virus1Satellite(String HostName) {
-        //System.out.println(HostName);
-        VirusLijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus a = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(a.getHostName())) {
-                if (a.getLineage().contains("Satellites")) {
-                    VirusLijst.add(a.getID());
-                }
-
-            }
-        }
-        //System.out.println(VirusLijst);
-        return VirusLijst;
-    }
-
-    public static ArrayList<String> Virus1Viroids(String HostName) {
-        //System.out.println(HostName);
-        VirusLijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus a = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(a.getHostName())) {
-                if (a.getLineage().contains("Viroids")) {
-                    VirusLijst.add(a.getID());
-                }
-
-            }
-        }
-        //System.out.println(VirusLijst);
-        return VirusLijst;
-    }
-
-    public static ArrayList<String> Virus1Unclassified(String HostName) {
-        //System.out.println(HostName);
-        VirusLijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus a = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(a.getHostName())) {
-                if (a.getLineage().contains("unclassified")) {
-                    VirusLijst.add(a.getID());
-                }
-
-            }
-        }
-        //System.out.println(VirusLijst);
-        return VirusLijst;
-    }
-
-    ////////////////////////////////
-    public static ArrayList<String> Virus2(String HostName) {
-        //System.out.println(HostName);
-        Virus2Lijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus b = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(b.getHostName())) {
-
-                Virus2Lijst.add(b.getID());
-            }
-
-        }
-        //System.out.println(VirusTempLijst);
-        return Virus2Lijst;
-    }
-
-    public static ArrayList<String> Virus2SSDNA(String HostName) {
-        //System.out.println(HostName);
-        Virus2Lijst.clear();
-        for (int i = 1; i < Lijst.size(); i++) {
-            Virus b = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(b.getHostName())) {
-                if (b.getLineage().contains("ssDNA")) {
-                    Virus2Lijst.add(b.getID());
-                }
-            }
-        }
-        //System.out.println(VirusTempLijst);
-        return Virus2Lijst;
-    }
-
-    public static ArrayList<String> Virus2DSDNA(String HostName) {
-        //System.out.println(HostName);
-        Virus2Lijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus b = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(b.getHostName())) {
-                if (b.getLineage().contains("dsDNA")) {
-                    Virus2Lijst.add(b.getID());
-                }
-            }
-        }
-        //System.out.println(VirusTempLijst);
-        return Virus2Lijst;
-    }
-
-    public static ArrayList<String> Virus2SSRNA(String HostName) {
-        //System.out.println(HostName);
-        Virus2Lijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus b = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(b.getHostName())) {
-                if (b.getLineage().contains("ssRNA")) {
-                    Virus2Lijst.add(b.getID());
-                }
-            }
-        }
-        //System.out.println(VirusTempLijst);
-        return Virus2Lijst;
-    }
-
-    public static ArrayList<String> Virus2DSRNA(String HostName) {
-        //System.out.println(HostName);
-        Virus2Lijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus b = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(b.getHostName())) {
-                if (b.getLineage().contains("dsRNA")) {
-                    Virus2Lijst.add(b.getID());
-                }
-            }
-        }
-        //System.out.println(VirusTempLijst);
-        return Virus2Lijst;
-    }
-
-    public static ArrayList<String> Virus2Retro(String HostName) {
-        //System.out.println(HostName);
-        Virus2Lijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus b = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(b.getHostName())) {
-                if (b.getLineage().contains("Retro-transcribing viruses")) {
-                    Virus2Lijst.add(b.getID());
-                }
-            }
-        }
-        //System.out.println(VirusTempLijst);
-        return Virus2Lijst;
-    }
-
-    public static ArrayList<String> Virus2Satellite(String HostName) {
-        //System.out.println(HostName);
-        Virus2Lijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus b = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(b.getHostName())) {
-                if (b.getLineage().contains("Satellites")) {
-                    Virus2Lijst.add(b.getID());
-                }
-            }
-        }
-        //System.out.println(VirusTempLijst);
-        return Virus2Lijst;
-    }
-
-    public static ArrayList<String> Virus2Viroids(String HostName) {
-        //System.out.println(HostName);
-        Virus2Lijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus b = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(b.getHostName())) {
-                if (b.getLineage().contains("Viroids")) {
-                    Virus2Lijst.add(b.getID());
-                }
-            }
-        }
-        //System.out.println(VirusTempLijst);
-        return Virus2Lijst;
-    }
-
-    public static ArrayList<String> Virus2Unclassified(String HostName) {
-        //System.out.println(HostName);
-        Virus2Lijst.clear();
-        for (int i = 0; i < Lijst.size(); i++) {
-            Virus b = Lijst.get(i);
-            //System.out.println(a);
-            if (HostName.equals(b.getHostName())) {
-                if (b.getLineage().contains("unclassified")) {
-                    Virus2Lijst.add(b.getID());
-                }
-            }
-        }
-        //System.out.println(VirusTempLijst);
-        return Virus2Lijst;
-    }
-*/
+    //process knop is ingedrukt
     public static ArrayList<String> Test(int J, int K, int L, String HostName, String Class) {
+        try{
         VirusLijst.clear();
 
         for (int i = 0; i < Lijst.size(); i++) {
@@ -404,13 +105,17 @@ public class Logic {
         if (K==1){
             Collections.sort(VirusLijst);
         }
-        
+        }
+        catch(Exception ex){
+            JOptionPane.showMessageDialog(null, "Virus ID's verkrijgen");
+        }
         return VirusLijst;
     }
 
    
-
+//overeenkomsten
     static ArrayList<String> Compare(ArrayList<String> VirusLijst1, ArrayList<String> VirusLijst2) {
+        try{
         CompareList.clear();
         for (int i = 0; i < VirusLijst1.size(); i++) {
             for (int k = 0; k < VirusLijst2.size(); k++) {
@@ -419,11 +124,11 @@ public class Logic {
                 }
             }
         }
-        
+        }
+        catch(Exception ex){
+            JOptionPane.showMessageDialog(null, "fout bij de overeenkomsten bepalen");
+        }
         return CompareList;
     }
-    
-    
-    
     
 }
